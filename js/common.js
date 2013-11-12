c4694512ac4810eeafc4674b879038d70e4bf256
huangtao800 <huangtao800@gmail.com>
2013-11-12 14:37:56 +0800

    finish askQuestion button

--- js/common.js
+++ js/common.js
@@ -1,6 +1,23 @@
 
 function askQuestion(){
-	window.open("../askQ/ask.html");
+	var cookie=document.cookie;
+	var arrcookie=cookie.split(";");
+	var userid;
+	for(var i=0;i<arrcookie.length;i++){
+		var arr=arrcookie[i].split["="];
+		if(arr=="id"){
+			userid=arr[1];
+			break;
+		}
+	}
+
+	
+	if(userid){
+		window.open("../askQ/ask.html");
+	}else{
+		window.open("../login/login.php");
+	}
+	
 }
 
 document.getElementById("askButton").onclick=askQuestion;
