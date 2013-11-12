a206cbd5f0a1040772c59ba15b42909a9b24c504
huangtao800 <huangtao800@gmail.com>
2013-11-12 17:04:53 +0800

    function printQuestion

--- include/useful.inc.php
+++ include/useful.inc.php
@@ -6,5 +6,29 @@ function getUserNameByID($userID){
 	return $row['name'];
 }
 
-function printQuestion($title,$answerCount,$userName){}
+function printQuestion($title,$answerCount,$userName){
+	print ("<div class='row rowTD'>
+          <div class='col-md-12'>
+            <div class='questionItem'>
+            <table>
+              <tr>
+                <td class='answerCountTD'>
+                  <div class='answerCount'>$answerCount</div>
+                  <div><small>Answers</small></div>
+                </td>
+                <td class='questionTD'>
+                  <a href=''><h4>$title</h4></a>
+                  <span class='label label-info labelTag'><a href='#' class='tagLink'>C++</a></span>
+                  <span class='label label-info labelTag'><a href='#' class='tagLink'>Java</a></span>
+                  <span class='label label-info labelTag'><a href='#' class='tagLink'>PHP</a></span>
+                </td>
+                <td class='userTD'>
+                  $userName
+                </td>
+              </tr>
+            </table> 
+            </div><!--questionItem ends-->
+          </div>
+        </div>");
+}
 ?>
