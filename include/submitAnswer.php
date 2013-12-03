ec960fe73ba0201b0d9c96fb9fc2bc761b9dfb7d
huangtao800 <huangtao800@gmail.com>
2013-12-03 15:58:15 +0800

    update answerCount

--- include/submitAnswer.php
+++ include/submitAnswer.php
@@ -11,6 +11,13 @@ if(isset($_POST['submitted'])){
 	$time=date('Y-m-d H:i:s');
 	$query="INSERT INTO answer(questionID,content,userID,time) VALUES ('$questionID','$answerContent','$userID','$time')";
 	mysqli_query($db,$query);
+	addAnswerCount($db,$userID);
 	header('Location: http://'. $_SERVER['HTTP_HOST'] ."/askQ/question.php?questionID=$questionID");
+}
+
+function addAnswerCount($db,$userID){
+  $query="UPDATE user set answerCount=answerCount+1 where id='$userID'";
+  $result=mysqli_query($db,$query);
+
 }
 ?>
